a2e99e804d96ed620e29c9918787cbf8ffde6b49
verolom21 <yuravasilev@mail.ru>
2021-02-05 20:43:02 +0300

    Первая тестовая работа Laravel

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+//Route::get('/', function () {
+//    return view('home');
+//});
+
+Route::get('/', 'MainController@index')->name('home');
+Route::get('/search', 'MainController@search')->name('search');
+
+
+Route::get('/about', function () {
+    return 'About page';
+});
+
